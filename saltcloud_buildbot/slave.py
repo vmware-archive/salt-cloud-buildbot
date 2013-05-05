84f1074fbe0967ddaaf2b32ae8ef6b24039c0027
Pedro Algarvio <pedro@algarvio.me>
2013-05-05 11:18:04 +0100

    A cleaner approach for waiting for the highstate.

--- saltcloud_buildbot/slave.py
+++ saltcloud_buildbot/slave.py
@@ -11,6 +11,7 @@
 '''
 
 # Import python libs
+import time
 import random
 import logging
 
@@ -264,41 +265,34 @@ class SaltCloudLatentBuildSlave(AbstractLatentBuildSlave):
 
         try:
             log.info('Running \'state.highstate\' on the minion')
-            #job_logger = logging.getLogger(
-            #    '{0}.saltclient'.format(__name__)
-            #)
+            job_logger = logging.getLogger(
+                '{0}.saltclient'.format(__name__)
+            )
             client = salt.client.LocalClient(
                 mopts=self._salt_master_config
             )
-            #output = client.cmd_iter(
-            #    self.saltcloud_vm_name, 'state.highstate',
-            #    timeout=999999999999
-            #)
-            #job = client.run_job(
-            #    self.saltcloud_vm_name,
-            #    'state.highstate',
-            #    timeout=999999999999
-            #)
-
-            #ret = {}
-            #for info in output:
-            #    if not info:
-            #        continue
-            #
-            #    job_logger.info(info)
-            #    ret.update(info)
-
-            #log.debug(ret)
-            #log.dbug(type(ret))
-            #ret = client.get_event_iter_returns(
-            #ret = client.get_cli_static_event_return(
-            #    job['job'],
-            #    job['minions'],
-            #    #timeout=999999999999
-            #)
-            ret = client.cmd_full_return(
-                self.saltcloud_vm_name,
+            job = client.run_job(
+                [self.saltcloud_vm_name],
                 'state.highstate',
+                expr_form='list',
+                timeout=9999999999999999,
+            )
+
+            while True:
+                running = client.cmd(
+                    'saltutil.is_running',
+                    [self.saltcloud_vm_name],
+                    arg=('state.highstate',),
+                    expr_form='list'
+                )
+                log.debug('IS RUNNING: {0}'.format(running))
+                if not running:
+                    break
+                time.sleep(1)
+
+            ret = client.get_full_returns(
+                job['jid'],
+                [self.saltcloud_vm_name],
                 timeout=5
             )
             try:
