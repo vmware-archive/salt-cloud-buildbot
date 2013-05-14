ab369593f8e9b85a0545f0138b74e92102b3a102
Pedro Algarvio <pedro@algarvio.me>
2013-05-14 19:16:27 +0100

    Add salt-call buildbot steps support

---
+++ saltcloud_buildbot/steps.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+'''
+    saltcloud_buildbot.steps
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Buildbot Steps
+
+    :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
+    :copyright: © 2013 by the SaltStack Team, see AUTHORS for more details.
+    :license: Apache 2.0, see LICENSE for more details.
+'''
+
+from buildbot.steps.shell import ShellCommand
+
+
+class SaltCallCommand(ShellCommand):
+
+    logfiles = {'minion': '/var/log/salt/minion'}
+
+    def __init__(self, salt_call_args, **kwargs):
+        if isinstance(salt_call_args, basestring):
+            salt_call_args = salt_call_args.split()
+        kwargs['command'] = ['salt-call'] + salt_call_args
+        ShellCommand.__init__(self, **kwargs)
+
+
+class SaltStateCommand(SaltCallCommand):
+
+    def __init__(self, state_name, salt_call_args=None, **kwargs):
+        if isinstance(salt_call_args, basestring):
+            salt_call_args = salt_call_args.split()
+        command = ['salt-call']
+        command.extend(salt_call_args)
+        command.extend(['state.single', state_name])
+        kwargs['command'] = command
+        ShellCommand.__init__(self, **kwargs)
